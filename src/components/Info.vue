5bd98792118dd313c8dd7fde8603dc3bdacf5356
Fabian Meyertöns <fabian.meyertoens@gmx.de>
2019-12-02 16:52:28 +0100

    add info page instead of generated home page

---
+++ src/components/Info.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>GraphQL - REST API Vergleich</h1>
+    <p>Diese Webanwendung wurde im Rahmen der Bachelorarbeit "Vergleich der Web API Ansätze REST und GraphQL" an der HTW Dresden entwickelt.</p>
+    <p>Anhand verschiedener Szenarien kann die Abfrageperformance von GraphQL und REST verglichen werden.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'info'
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home mx-auto">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Info/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Info from '@/components/Info.vue'; // @ is an alias to /src
 
 export default Vue.extend({
   name: 'home',
   components: {
-    HelloWorld,
+    Info,
   },
 });
 </script>
